8604970277504ffd2d3b076e10b3921001b56194
Sagar <sagarpatkar17@gmail.com>
2020-11-17 21:09:34 +0530

    Add models file, add seeder.js

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoosse from "mongoose";
+
+const userSchema = new mongoosse.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+}, {timestamps: true});
+
+const User = mongoosse.model("User", userSchema);
+
+export default User;
