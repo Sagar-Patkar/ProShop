4d52e267c0990f1f6a546cdbbfa66dcb97c686cf
Sagar <sagarpatkar17@gmail.com>
2020-12-01 12:43:17 +0530

    add middleware file and add custom error handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 const app = express();
 dotenv.config();
@@ -10,6 +11,8 @@ app.get("/", (req,res) => {
     res.send("API is running...");
 });
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`);
 })
