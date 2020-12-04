583d0ef8d47dcd7e05aa1a18f6287be7246f9450
Sagar <sagarpatkar17@gmail.com>
2020-12-04 12:02:43 +0530

    Backend User Authentication (new file :- productController.js, userController.js, authMiddleware.js, userRoutes.js, generateToken.js; modified file :- userModel.js, productRoutes.js, server.js)

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,16 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const app = express();
 dotenv.config();
+app.use(express.json())
 connectDB()
 app.get("/", (req,res) => {
     res.send("API is running...");
 });
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 app.listen(process.env.PORT || 5000, () => {
