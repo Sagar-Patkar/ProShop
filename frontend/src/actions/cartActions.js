18966bcacce82a3e8a5170f57bbf56b8b86bf842
Sagar <sagarpatkar17@gmail.com>
2021-01-11 17:25:04 +0530

    Shipping Screen & Save Address (new file: ShippingScreen.jsx), (modified: App.js, cartActions.js, cartConstants.js, cartReducers.js, store.js)

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants.js";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants.js";
 import axios from "axios";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -24,4 +24,12 @@ export const removeFromCart = (id) => (dispatch, getState) => {
     })
     localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems
     ))
+} 
+
+export const saveShippingAddress = (data) => (dispatch) => {
+    dispatch({
+        type: CART_SAVE_SHIPPING_ADDRESS,
+        payload: data
+    })
+    localStorage.setItem("shippingAddress", JSON.stringify(data))
 } 
