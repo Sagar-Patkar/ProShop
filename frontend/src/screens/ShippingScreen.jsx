0f7f10225ed4bafc200641807642f18659c44df8
Sagar <sagarpatkar17@gmail.com>
2021-01-11 21:41:34 +0530

    Checkout Steps Component, Payment Screen & Save Payment Method (new file: CheckoutSteps.jsx, PaymentScreen.jsx), (modified: App.js, cartActions.js, cartConstants.js, cartReducers.js, ShippingScreen.jsx)

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer.jsx";
+import CheckoutSteps from "../components/CheckoutSteps.jsx";
 import { saveShippingAddress } from "../actions/cartActions.js";
 
 const ShippingScreen = ({ history }) => {
@@ -19,6 +20,7 @@ const ShippingScreen = ({ history }) => {
     }
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2 />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId="address">
