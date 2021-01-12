1d3b3566bb7c4b05c48971d358ecfde34d1943f2
Sagar <sagarpatkar17@gmail.com>
2021-01-13 02:05:12 +0530

    Order Controller & Route & Create Order (new file: orderController.js, orderRoutes.js, orderActions.js, orderConstants.js, orderReducers.js), (modified: server.js, PlaceOrderScreen.jsx, store.js)

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { Button, Row, Col, Image, ListGroup, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message.jsx";
 import CheckoutSteps from "../components/CheckoutSteps.jsx";
+import { createOrder } from "../actions/orderActions.js";
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
+    const dispatch = useDispatch();
     const cart = useSelector(state => state.cart);
+    const orderCreate = useSelector(state => state.orderCreate);
+    const { order, success, error } = orderCreate;
+    useEffect(() => {
+        if(success){
+            history.push(`/order/${order._id}`)
+        }
+        // eslint-disable-next-line
+    },[history, success])
     const placeOrderHandler = () => {
-        console.log("order");
+        dispatch(createOrder({
+            orderItems: cart.cartItems,
+            shippingAddress: cart.shippingAddress,
+            paymentMethod: cart.paymentMethod,
+            itemsPrice: cart.itemsPrice,
+            shippingPrice: cart.shippingPrice,
+            taxPrice: cart.taxPrice,
+            totalPrice: cart.totalPrice,
+        }))
     }
     const addDecimals = (num) => {
         return (Math.round(num * 100) / 100).toFixed(2)
@@ -107,6 +125,9 @@ const PlaceOrderScreen = () => {
                                         ${cart.totalPrice}
                                     </Col>
                                 </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {error && <Message variant="danger">{error}</Message>}
                             </ListGroup.Item>
                             <ListGroup.Item>
                                 <Button type="button" className="btn-block" disabled={cart.cartItems === 0} onClick={placeOrderHandler}>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer, productDetailsReducer } from "./reducers/productReducers.js";
 import { cartReducer } from "./reducers/cartReducers.js";
+import { orderCreateReducer } from "./reducers/orderReducers.js";
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from "./reducers/userReducers.js";
 
 const reducer = combineReducers({
@@ -13,6 +14,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
 });
 const cartItemsFromStorage = localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")):[];
 const userInfoFromStorage = localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")):null;
