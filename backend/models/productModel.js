4c6c83816953273bf9f9a0bc0e531a71b3189e1a
Sagar <sagarpatkar17@gmail.com>
2021-01-25 20:49:58 +0530

    Morgan package add & Create Review Endpoint (modified: productController.js, productModel.js, productRoutes.js, server.js)

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,68 +1,79 @@
 import mongoosse from "mongoose";
 
-const reviewSchema = new mongoosse.Schema({
-    name:{
-        type: String, 
-        required: true
+const reviewSchema = new mongoosse.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
     rating: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     comment: {
-        type: String,
-        required: true
-    }
-},{timestamps: true}); 
-
-const productSchema = new mongoosse.Schema({
+      type: String,
+      required: true,
+    },
     user: {
-        type: mongoosse.Schema.Types.ObjectId,
-        required: true,
-        ref: "User"
+      type: mongoosse.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new mongoosse.Schema(
+  {
+    user: {
+      type: mongoosse.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     image: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     brand: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     category: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     reviews: [reviewSchema],
     rating: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
     numReviews: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
     price: {
-        type: Number,
-        required: true,
-        default: 0
+      type: Number,
+      required: true,
+      default: 0,
     },
     countInStock: {
-        type: Number,
-        required: true,
-        default: 0
-    }
-}, {timestamps: true});
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
 const Product = mongoosse.model("Product", productSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import path from "path";
+import morgan from "morgan";
 const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 dotenv.config();
 app.use(express.json());
 connectDB();
